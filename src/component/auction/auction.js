1902d7e7a82fd38e03ed2aceb18ca87a18c173db
floraPri <siskrin96@gmail.com>
2023-10-11 03:01:15 +0900

    2023.10.11.AM03:11

--- src/component/auction/auction.js
+++ src/component/auction/auction.js
@@ -1,8 +1,13 @@
 import Card from 'react-bootstrap/Card';
 import Link from "next/link";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import React, { useEffect, useState, useCallback } from 'react';
 import styled from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRouter } from "next/router";
 
 function Auction() {
 
@@ -12,11 +17,55 @@ function Auction() {
     "성북구", "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"
   ];
   
-  const Container = styled.div`
+  const Container__1 = styled.div`
+    display: flex;      /* 한줄(수평)로 배치 */
+    justify-content: space-between;
+  `;
+
+  const Container__2 = styled.div`
+    width: 15%;
+    height: 10px;
+  `;
+
+  const Container__3 = styled.div`
+    width: 85%;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #000;
+  `;
+
+    const Acu = styled.div`
+    margin-top: 10px;
+    margin-left: 200px;
+    text-decoration: none;
+  `;
+
+
+  const Container = styled.div`
+    margin-top: 10px; /* 위쪽 10px 마진 */
+    margin-bottom: 10px; /* 아래쪽 10px 마진 */
+    margin-right: 50px; /* 오른쪽 50px 마진 */
+    margin-left: 50px; /* 왼쪽 50px 마진 */
   `;
   
+  const AuctionDiv__1 = styled.div`
+    padding-right: 5px;
+    color: green;
+    white-space: nowrap;
+  `;
+
+  const AuctionDiv = styled.div`
+    display: flex;
+    font-weight: bold;
+    width : 10px;
+    font-size: 22px;
+  `;
+
+  const ConstituencyDiv = styled.div`
+    font-size: 19px;
+  `;
+    
   const Cap = styled.img`
     width: 1800px;
   `;
@@ -27,107 +76,60 @@ function Auction() {
     setSelectedOption(event.target.value);
   };
 
-  const [data, setData] = useState([]); // 데이터를 저장할 상태
-
-  const [isLoading, setIsLoading] = useState(false); // 데이터 로딩 중 여부
-
-    // 초기 데이터 로딩
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-    // 스크롤 위치 감지
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
-  // 스크롤 위치에 따라 추가 데이터 로딩
-  const handleScroll = () => {
-    if (
-      window.innerHeight + window.scrollY >=
-      document.documentElement.offsetHeight - 100
-    ) {
-      // 스크롤 위치가 페이지 하단에 가까워지면 데이터 추가 로딩
-      fetchData();
-    }
-  };
-
-  const fetchData = async () => {
-    if (!isLoading) {
-      setIsLoading(true);
-  
-      // 실제 데이터를 가져오는 비동기 요청 코드를 여기에 구현
-      // 예: const response = await fetch('API_ENDPOINT');
-      // const newData = await response.json();
-  
-      // 임시로 setTimeout을 사용한 예제
-      setTimeout(() => {
-        const newData = Array.from({ length: 10 }, (_, index) => index + data.length);
-        setData([...data, ...newData]);
-        setIsLoading(false);
-      }, 1000);
-    }
-  };
-  
-  // 데이터 추가 로딩 함수
+  const router = useRouter();
 
   return (
     <Container>
-      <div class="auctionDiv">
-        3 개
-        {/* <Link href="/auction/auctionDetail">상세페이지</Link> */}
-      </div>
-      <div class="constituency">
-        <select value={selectedOption} onChange={handleSelectChange}>
-          <option value="">지역구</option>
-          {regions.map((region, index) => (
-            <option key={index} value={region}>
-              {region}
-            </option>
-          ))}
-        </select>
-      </div>
-			{/* <div class="acu"> 
-       <Link href="/auction/auctionDetail">
-				<div class="ra"> */}
-          {/* <div> 이미지 삽입 칸 </div> */}
-					{/* <div class="card-body">
-						<h4><p class="card-text">자퇴서</p></h4>
-						<ul>
-							<li>참여자 : 55 명</li>
-							<li>시작금액 : 650000 원</li>
-              <li>경매 남은 시간 : 55 : 38 : 51 : 13</li>
-						</ul>
-					</div>	
-				</div>
-        </Link>
-			</div> */}
-      {/* <Cap src="/assets/images/auction/auction.PNG" /> */}
-      <div class="acu">
-        <Link href="/auction/auctionDetail">
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>자퇴서</Card.Title>
-              <Card.Text>
-                참여자 : 55 명<br/>
-                시작금액 : 650000 원<br/>
-                경매 남은 시간 : 55 : 38 : 51 : 13<br/>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Link>
-      </div>
+      <Container__1>
+        <Container__2>
+          <ButtonGroup vertical>
+          <Button variant="success" onClick={() => router.push('/auction/auction') } >오늘의 경매 PARTY</Button>
+
+          <DropdownButton variant="success"
+            as={ButtonGroup}
+            title="나의 경매"
+            id="bg-vertical-dropdown-1"
+          >
+            <Dropdown.Item eventKey="1">HOST</Dropdown.Item>
+            <Dropdown.Item eventKey="2">GUEST</Dropdown.Item>
+          </DropdownButton>
+          </ButtonGroup>
+        </Container__2>
+        <Container__3>
+          <AuctionDiv className="auctionDiv">
+            <AuctionDiv__1 className="auctionDiv__1">3</AuctionDiv__1>개
+          </AuctionDiv>
+          <ConstituencyDiv class="constituency">
+            <select value={selectedOption} onChange={handleSelectChange}>
+              <option value="">지역구</option>
+              {regions.map((region, index) => (
+                <option key={index} value={region}>
+                  {region}
+                </option>
+              ))}
+            </select>
+          </ConstituencyDiv>
+        </Container__3>
+      </Container__1>  
+        <Acu class="acu">
+          <Link href="/auction/auctionDetail">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="/assets/images/auction/ac1.PNG" />
+              <Card.Body>
+                <Card.Title>자퇴서</Card.Title>
+                <Card.Text>
+                  참여자 : 55 명<br/>
+                  시작금액 : 650000 원<br/>
+                  경매 남은 시간 : 55 : 38 : 51 : 13<br/>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Acu>
     </Container>
   );
 }
 
-
-// export default Auction;
-// export { BasicExample }; // BasicExample 컴포넌트를 export
 export default Auction;
 
 
