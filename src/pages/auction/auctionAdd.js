5dac244692103a27e57f71ee23cc582db221e7a5
floraPri1~ <siskrin96@gmail.com>
2023-10-13 15:51:13 +0900

    2023.10.13.PM03:51

---
+++ src/pages/auction/auctionAdd.js
@@ -0,0 +1,11 @@
+import AuctionAdd from "@/component/auction/auctionAdd"
+//import BasicExample from "@/component/auction/auction"
+//import { Auction , BasicExample } from "@/component/auction/auction"
+
+export default function test ({children}){
+    return(
+        <div>
+            <AuctionAdd />
+        </div>
+    )
+}
---
+++ src/pages/auction/auctionEdit.js
@@ -0,0 +1,11 @@
+import AuctionEdit from "@/component/auction/auctionEdit"
+//import BasicExample from "@/component/auction/auction"
+//import { Auction , BasicExample } from "@/component/auction/auction"
+
+export default function test ({children}){
+    return(
+        <div>
+            <AuctionEdit />
+        </div>
+    )
+}
