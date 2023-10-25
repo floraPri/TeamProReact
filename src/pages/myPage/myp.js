eff96fe62a9ab47aa68d97a400272e640cb6a227
KMJ <iris88819@naver.com>
2023-10-25 14:50:22 +0900

    myfeedPage

--- src/pages/myPage/myp.js
+++ src/pages/myPage/myp.js
@@ -8,6 +8,7 @@ import axios from "axios";
 import { MdDeleteForever } from "react-icons/md";
 import { RiPencilFill } from "react-icons/Ri";
 import { getAuthToken } from "@/component/user/axios_helper";
+import {Table ,TableCell, TableRow, TableBody, Button, TableContainer} from "@mui/material";
 
 const Container = styled.div`
   min-width: 1280px;
@@ -20,7 +21,7 @@ const Container = styled.div`
 const RightContainer = styled.div`
     width: 960px;
     padding-left: 50px;
-
+    objectFit="cover"
 `;
 
 const RightInnerWrap = styled.div`
@@ -35,10 +36,20 @@ const RightInnerTitle = styled.div`
     padding-bottom: 20px;
 `;
 
+const ThumImage = styled.img`
+  width: 250px; 
+  height: auto;
+`;
+
 export default function MyPages (){
+
   const [userno,setUserno] = useState("");
   const [email,setEmail] = useState("");
   const [feeds,setFeeds] = useState([]);
+
+  const deleteFeed = (feedcode) => {
+    console.log(feedcode);
+  };
 
   const formatTimeStamp = (timestamp) => {
     const options = {
@@ -82,53 +93,49 @@ export default function MyPages (){
     }
   },[]);
 
-    return(
-      <Container>
-        <MyPagesMenu />
-        <RightContainer>
-            {/** 마이페이지 메인1 */}
-            <h3 className={rightStyles.welcomeTitle}><b>{email}</b>님 안녕하세요!</h3>
-            <div className={rightStyles.welcomeBox}>
-                <p>내가 작성한 게시글<span className={rightStyles.txtNumber}>52</span></p>
-                <p>팔로워<span className={rightStyles.txtNumber}>20</span></p>
-                <p>팔로잉<span className={rightStyles.txtNumber}>109</span></p>
-            </div>
+  return(
+    <Container>
+      <MyPagesMenu />
+      <RightContainer>
+          {/** 마이페이지 메인1 */}
+          <h3 className={rightStyles.welcomeTitle}><b>{email}</b>님 안녕하세요!</h3>
+          <div className={rightStyles.welcomeBox}>
+              <p>내가 작성한 게시글<span className={rightStyles.txtNumber}>52</span></p>
+              <p>팔로워<span className={rightStyles.txtNumber}>20</span></p>
+              <p>팔로잉<span className={rightStyles.txtNumber}>109</span></p>
+          </div>
 
-            {/** 마이페이지 내 피드 */}
-            <RightInnerWrap>
-                <RightInnerTitle>
-                    <h3 className={rightStyles.h3_title}>{email}님 피드</h3> <p><Link href="">더 보기</Link></p>
-                </RightInnerTitle>
-                <ul className={rightStyles.feedWrapUl}>
-                    {feeds.map((feed,index) => (
-                      
-                      <li key={index}>
-                        <div className={rightStyles.liInner}>
-                          <h3>{feed.feedtitle}</h3>
-                          <p className={rightStyles.txtDate}>{formatTimeStamp(feed.feedregdate)}</p>
-                          <p className={rightStyles.txtArea}>
-                            {feed.feedcontent}
+          {/** 마이페이지 내 피드 */}
+          <RightInnerWrap>
+              <RightInnerTitle>
+                  <h3 className={rightStyles.h3_title}>{email}님 피드</h3> <p><Link href="">더 보기</Link></p>
+              </RightInnerTitle>
+              <ul className={rightStyles.feedWrapUl}>
+                  {feeds.map((feed,index) => (
+                    
+                    <li key={index}>
+                      <div className={rightStyles.liInner}>
+                        <h3>{feed.feedtitle}</h3>
+                        <p className={rightStyles.txtDate}>{formatTimeStamp(feed.feedregdate)}</p>
+                        <p className={rightStyles.txtArea}>
+                          {feed.feedcontent}
+                        </p>
+                        <p className={rightStyles.imgWrap}>
+                          <ThumImage 
+                            src={feed.feedimg}
+                            alt={feed.feedtitle} />
                           </p>
-                          <p className={rightStyles.imgWrap}>
-                            <Image 
-                              src={feed.feedimg.replace('http://localhost:8081', '')}
-                              width="250" 
-                              height="190"
-                              objectFit="cover" 
-                              alt={feed.feedtitle} />
-                            </p>
-                            {feed.feedimg}
-                            <div className={rightStyles.btnWarp}>
-                              <span><MdDeleteForever size="24" /></span>
-                              <span><RiPencilFill size="24" /></span>
-                            </div>
-                        </div>
-                      </li>
-                    ))}
-                </ul>
-            </RightInnerWrap>           
-        </RightContainer>        
-        {/** <MyPageRight /> */}
-      </Container>
-    )
+                          <div className={rightStyles.btnWarp}>
+                            <Button><RiPencilFill size="24" color="#7b7b7b" /></Button>
+                            <Button><MdDeleteForever size="24" color="#7b7b7b" /></Button> 
+                          </div>
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+          </RightInnerWrap>           
+      </RightContainer>        
+      {/** <MyPageRight /> */}
+    </Container>
+  )
 }
